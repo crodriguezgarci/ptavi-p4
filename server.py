d5fe2363dafa86ad3f8b02ec54a00d20d39bfecf
carlos rodriguez garcia <crodrigu@iota08.aulas.gsyc.es>
2014-10-20 10:53:45 +0200

    Version actual server.py : Ejercicio 5 finalizado

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ en UDP simple
 """
 
 import SocketServer
+import sys
 
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
@@ -15,6 +16,10 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
+        
+        print "Petición recibida del Cliente: ",
+        print "IP:" + str(self.client_address[0]),
+        print " Puerto: " + str(self.client_address[1])
         self.wfile.write("Hemos recibido tu peticion")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
@@ -25,7 +30,8 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
             
 
 if __name__ == "__main__":
+    PORT = int(sys.argv[1])
     # Creamos servidor de eco y escuchamos
-    serv = SocketServer.UDPServer(("", 6001), EchoHandler)
+    serv = SocketServer.UDPServer(("", PORT ), EchoHandler)
     print "Lanzando servidor UDP de eco..."
     serv.serve_forever()
