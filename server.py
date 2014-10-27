4bc5b5e0e2224abc4e929e16d4220933c88d7c80
carlos rodriguez garcia <crodrigu@iota07.aulas.gsyc.es>
2014-10-27 10:03:36 +0100

    Version actual server.py : Ejercicio 8 finalizado

--- server.py
+++ server.py
@@ -7,13 +7,31 @@ en UDP simple
 
 import SocketServer
 import sys
+import time
 
 registro = {}
+
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
     """
     Echo server class
     """
+    def borrar_caducados(self,registro):
+        self.lista_aux = []
+        for key in registro:
+            if time.time() >= float(registro[key][2]) + float(registro[key][1]):
+               	self.lista_aux.append(key)
+        for key in self.lista_aux:
+            del registro[key]
+
+    def register2file(self,registro):
+        fichero = open("registered.txt", "w")
+        fichero.write("User \t IP \t Expires \n")
+        for key in registro:
+            fichero.write(key + " " + registro[key][0] + " ")
+            fecha = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(registro[key][2]))
+            fichero.write (fecha + "\n")
+
 
     def handle(self):
 
@@ -26,15 +44,19 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
             if not line:
                 break
             line = line.split()
-            if line[0] == "REGISTRER" and line[2] == "SIP/1.0":
+            if line[0] == "REGISTER" and line[2] == "SIP/1.0":
                 line[1] = line[1].split(":")
                 if line[4] != "0":
-                    registro[line[1][1]] = [str(self.client_address[0]),line[4]]
+                    fecharegistro = time.time()
+                    registro[line[1][1]] = [str(self.client_address[0]),line[4], fecharegistro]
                     self.wfile.write(line[2] + "200 OK\r\n\r\n")
                 else:
                     self.wfile.write(line[2] + "200 OK\r\n\r\n")
                     if line[1][1] in registro:
-                        del registro[line[1][1]] 
+                        del registro[line[1][1]]
+            self.borrar_caducados(registro)
+            self.register2file(registro)
+
 if __name__ == "__main__":
     PORT = int(sys.argv[1])
     # Creamos servidor de eco y escuchamos
